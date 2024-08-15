aeed6e0e844d04e2001bd2c4cd4ff74c1aa9d271
Frank Krick <frank.krick@gmail.com>
2024-08-15 17:08:29 -0400

    Added config parameters and files

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use home::home_dir;
 use serde::{Deserialize, Serialize};
 use std::fs::{self, File};
 use std::io::Read;
@@ -48,6 +49,41 @@ pub fn read_service_urls() -> ServiceUrls {
             let default = ServiceUrls {
                 pipewire_registry_url: String::from("http://127.0.0.1:50000"),
                 pmx_registry_url: String::from("http://127.0.0.1:50001"),
+            };
+            fs::write(path, toml::to_string_pretty(&default).unwrap()).unwrap();
+            default
+        }
+        Ok(mut file) => {
+            let mut raw_string = String::new();
+
+            if let Err(why) = file.read_to_string(&mut raw_string) {
+                panic!("couldn't read {}: {}", display, why);
+            }
+
+            toml::from_str(raw_string.as_str()).unwrap()
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct DataFiles {
+    pub pmx_registry_data_file: String,
+}
+
+pub fn read_data_file_paths() -> DataFiles {
+    let home_dir = home::home_dir().unwrap();
+    let path = home_dir.join(".config/pmx-1/data_files.toml");
+    let display = path.display();
+
+    match File::open(&path) {
+        Err(_why) => {
+            let default = DataFiles {
+                pmx_registry_data_file: String::from(
+                    home_dir
+                        .join(".config/pmx-1/pmx_registry.json")
+                        .to_str()
+                        .unwrap(),
+                ),
             };
             fs::write(path, toml::to_string_pretty(&default).unwrap()).unwrap();
             default
